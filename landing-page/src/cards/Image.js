9e5d9cfbaf8758ff85fa31416c2e94373598d6df
Mac <abdullah.faisal@qbatch.com>
2023-07-24 10:42:32 +0500

    modified filing

--- landing-page/src/cards/Image.js
+++ landing-page/src/cards/Image.js
@@ -1,5 +1,4 @@
 import React from "react";
-import image from "./../assets/image.jpg";
 import "./../styles/Image.css";
 
 const Image = (props) => {
@@ -17,12 +16,9 @@ const Image = (props) => {
       <div className="mt-2.5">
         <h1 className="max-w-md text-2xl font-bold mt-2.5 img-desc">
           {props.title}
-          {/* Advanced Features */}
         </h1>
         <p className="max-w-sm text-darkGrayishBlue mt-2.5">
           {props.description}
-          {/* Our landing page template works on all devices, so you only have to
-          set it up once and get beautiful results forever */}
         </p>
       </div>
     </div>
